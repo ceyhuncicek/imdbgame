c47c00e4e2854311db0bdbf6c5f415bc19eeb561
Ceyhun <40377224+1Ceko@users.noreply.github.com>
2019-06-12 01:11:24 +0300

    Some style update

--- app.js
+++ app.js
@@ -1,53 +1,46 @@
 const express = require('express');
 const app = express();
 const scraper = require('./scraper'); 
-let data = "";
 
-
-// var data = { title: 'The Martian',
-// year: '2015',
-// contentRating: 'PG-13',
-// runtime: '2h 24min',
-// description: 'During a manned mission to Mars, Astronaut Mark Watney is presumed dead after a fierce storm and left behind by his crew. But Watney has survived and finds himself stranded and alone on the hostile planet. With only meager supplies, he must draw upon his ingenuity, wit and spirit to subsist and find a way to signal to Earth that he is alive.',
-// rating: '8.1',
-// poster: 'https://m.media-amazon.com/images/M/MV5BMTc2MTQ3MDA1Nl5BMl5BanBnXkFtZTgwODA3OTI4NjE@._V1_SY1000_CR0,0,675,1000_AL_.jpg',
-// genre: [ 'Adventure', ' Drama', ' Sci-Fi' ],
-// director: 'Ridley Scott',
-// metascore: '80',
-// writer: 'Drew Goddard' };
-
-console.log(data.title);
-
-
+//set view engine as ejs
 app.set('view engine', 'ejs');
 
-
+//what server will do in get request
 app.get('/', (req, res ) => {
 
-    const url = 'tt3659388';
-//give url as movie id and get back movie details
-scraper.getDetails(url)
-.then((data)=>{
-        console.log(data);
-        res.render ('index', {data: data,} );
-}).catch((err)=>{
-    console.log(err);
+
+    let data = { title: 'The Martian',
+    year: '2015',
+    description: 'During a manned mission to Mars, Astronaut Mark Watney is presumed dead after a fierce storm and left behind by his crew. But Watney has survived and finds himself stranded and alone on the hostile planet. With only meager supplies, he must draw upon his ingenuity, wit and spirit to subsist and find a way to signal to Earth that he is alive.',
+    rating: '8.1',
+    poster: 'https://m.media-amazon.com/images/M/MV5BMTc2MTQ3MDA1Nl5BMl5BanBnXkFtZTgwODA3OTI4NjE@._V1_SY1000_CR0,0,675,1000_AL_.jpg',
+    genre: [ 'Adventure', ' Drama', ' Sci-Fi' ]
+    }
+
+    res.render ('index', {data: data,} );
+
+
+
 });
 
-
- });
-
-
+//what server will do in post request
 app.post ('/', (req, res) => {
+    
+    const url = 'tt3659388';
+
+    //give url as movie id and get back movie details
+    scraper.getDetails(url)
+    .then((data)=>{
+            console.log(data);
+    }).catch((err)=>{
+        console.log(err);
+    });
     res.send("Post isteği gönderildi");
 });
 
 
 
-
-
+//while server is working on 3000 port give message
 app.listen(3000, () => {
-    console.log('express server is working')
-
+    console.log('Express server is working')
 });
-
