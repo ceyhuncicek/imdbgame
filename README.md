d256475263dfddf86a7f8933ec299478c2f0c979
Ceyhun <40377224+1Ceko@users.noreply.github.com>
2019-06-12 00:52:07 +0300

    Card added and connected
    -Card added
    -Api connected with card

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 I installed nodemon to make changes easier. 
 I start with vanilla node.js then i saw it is too hard response requestes one by one by vanilla node.js then i decided to use express.js for this routes.
 I decided to use .ejs html template engine to use html files and send datas to them easily.
+I used bootstrap and https://bootsnipp.com/snippets/z8PZx card template thanks to evarevirus
+
--- app.js
+++ app.js
@@ -1,36 +1,39 @@
 const express = require('express');
 const app = express();
 const scraper = require('./scraper'); 
+let data = "";
 
 
+// var data = { title: 'The Martian',
+// year: '2015',
+// contentRating: 'PG-13',
+// runtime: '2h 24min',
+// description: 'During a manned mission to Mars, Astronaut Mark Watney is presumed dead after a fierce storm and left behind by his crew. But Watney has survived and finds himself stranded and alone on the hostile planet. With only meager supplies, he must draw upon his ingenuity, wit and spirit to subsist and find a way to signal to Earth that he is alive.',
+// rating: '8.1',
+// poster: 'https://m.media-amazon.com/images/M/MV5BMTc2MTQ3MDA1Nl5BMl5BanBnXkFtZTgwODA3OTI4NjE@._V1_SY1000_CR0,0,675,1000_AL_.jpg',
+// genre: [ 'Adventure', ' Drama', ' Sci-Fi' ],
+// director: 'Ridley Scott',
+// metascore: '80',
+// writer: 'Drew Goddard' };
 
-var data = { title: 'The Martian',
-year: '2015',
-contentRating: 'PG-13',
-runtime: '2h 24min',
-description: 'During a manned mission to Mars, Astronaut Mark Watney is presumed dead after a fierce storm and left behind by his crew. But Watney has survived and finds himself stranded and alone on the hostile planet. With only meager supplies, he must draw upon his ingenuity, wit and spirit to subsist and find a way to signal to Earth that he is alive.',
-rating: '8.1',
-poster: 'http://ia.media-imdb.com/images/M/MV5BMTc2MTQ3MDA1Nl5BMl5BanBnXkFtZTgwODA3OTI4NjE@._V1_UX182_CR0,0,182,268_AL_.jpg',
-genre: [ 'Adventure', ' Drama', ' Sci-Fi' ],
-director: 'Ridley Scott',
-metascore: '80',
-writer: 'Drew Goddard' };
+console.log(data.title);
 
-//console.log(data.title);
-// const url = 'tt3659388';
-// //give url as movie id and get back movie details
-// scraper.getDetails(url)
-// .then((data)=>{
-//     console.log(data);
-// }).catch((err)=>{
-//     console.log(err);
-// });
 
 app.set('view engine', 'ejs');
 
 
 app.get('/', (req, res ) => {
-    res.render ('index', {data: data,} );
+
+    const url = 'tt3659388';
+//give url as movie id and get back movie details
+scraper.getDetails(url)
+.then((data)=>{
+        console.log(data);
+        res.render ('index', {data: data,} );
+}).catch((err)=>{
+    console.log(err);
+});
+
 
  });
 
