142dc38e69ef5c4665f343477a7e63569433be70
Ceyhun <40377224+1Ceko@users.noreply.github.com>
2019-06-21 00:47:17 +0300

    socket.io completed
    - all data transfer has been socket.io

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser')
 const storage = require('node-persist');
-const scraper = require('./scraper'); 
+const scraper = require('./scraper');
 const ListScraper = require('./ListScraper');
 const helper = require('./helper');
 var http = require('http').createServer(app);
@@ -17,7 +17,7 @@ const urlencodedParser = bodyParser.urlencoded({ extended: false })
 //set view engine as ejs
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res ) => {
+app.get('/', (req, res) => {
 
 
     //get dummyData to show first time
@@ -25,150 +25,166 @@ app.get('/', (req, res ) => {
 
     res.sendFile(__dirname + "/Assest/" + "index.html");
 
- 
+
 });
 
-io.on('connection', function(socket){
+io.on('connection', function (socket) {
 
     let data = helper.DummyData;
 
 
-    socket.emit('news', data);
-    socket.on('my other event', function(data) {
-        console.log(data);
-    });
+    socket.emit('imdb-game', data);
 
 
 
-    console.log('a user connected');
-    socket.on('disconnect', function(){
-      console.log('user disconnected');
-    });
-  });
-
-app.get('/Options', (req, res ) => {
+    socket.on('guess', function (score) {
 
 
-    //get dummyData to show fist time
-    let data = helper.DummyData;
 
-    res.render ('options', {data: data,} );
+        async function runFunction() {
 
-});
+            //gate data value from storage
+            data = await storage.getItem('Currentdata');
 
-app.get('/Play', (req, res ) => {
+            //check if score is same with data.rating
+            helper.AnalyzeScore(score, data);
 
-
-    //get dummyData to show fist time
-    let data = helper.DummyData;
-
-    res.render ('play', {data: data,} );
-
-});
+            //render page with sending data
+            socket.emit('imdb-game', data);
 
 
-app.post ('/Play', (req, res) => {
-
-    async function SaveCurrentDataAsync() {
-        try {
-        // Currentdata = data;
-        await storage.setItem ('Currentdata', data );
-            console.log("Currentdata have been saved");
-        }catch{
-            console.log("Currentdata have not been saved!");
         }
-    }
 
-    async function GetScrapeData() {
+        //start async function
+        runFunction();
 
-        try {
-            //get scrapped data from storage if it is exist
-            data = await storage.getItem('Scrapedata');
-            console.log("Data has been taken from storage");
-            //get random movie data from randomer
-            data = helper.RandomPicker(data);
 
-            res.render ('play', {data: data,} );
-            SaveCurrentDataAsync(data);
-            }catch{
+
+        // console.log(score);
+    });
+
+
+
+
+    socket.on('Play', function () {
+
+        async function SaveCurrentDataAsync() {
+            try {
+                await storage.setItem('Currentdata', data);
+                console.log("Currentdata have been saved");
+            } catch{
+                console.log("Currentdata have not been saved!");
+            }
+        }
+
+        async function GetScrapeData() {
+
+            try {
+                //get scrapped data from storage if it is exist
+                data = await storage.getItem('Scrapedata');
+                console.log("Data has been taken from storage");
+                //get random movie data from randomer
+                data = helper.RandomPicker(data);
+
+                socket.emit('imdb-game', data);
+                SaveCurrentDataAsync(data);
+            } catch{
                 console.log("Error while getting Scrapedata");
             }
-    }
-    GetScrapeData();
- 
-});
+        }
+        GetScrapeData();
 
-app.post('/guess', urlencodedParser, function (req, res) {
-
-    //catch post value and name it as score
-    score = req.body.score;
-
-    async function runFunction() {
-
-        //gate data value from storage
-        data = await storage.getItem('Currentdata');
-
-        //check if score is same with data.rating
-        helper.AnalyzeScore(score,data);
-
-        //render page with sending data
-        res.render ('play', {data: data,} );
-    }
-
-    //start async function
-    runFunction();
-
-});
+    });
 
 
-app.post('/ListScraper', urlencodedParser, function (req, res) {
+    socket.on('Start', function (url) {
 
-        url = req.body.url;
 
         //scrap movie id's from this url
-        ListScraper(url, function(err, data) {
-            console.log("List Scrapper is Working");
+        ListScraper(url, function (err, data) {
             ScrabFunction(data);
-          });
-    
+        });
+
         //save scrapped data to storage
-        function ScrabFunction(data){ 
+        function ScrabFunction(data) {
             async function ScrapData() {
-                console.log(data);
-                await storage.init( /* options ... */ );
-                await storage.setItem('Scrapedata',data)
-                res.redirect('/Play')
-              }
-        ScrapData();
+                await storage.init( /* options ... */);
+                await storage.setItem('Scrapedata', data)
+                console.log("List have been scrapped");
+
+                //render page with sending data
+
+                let newData = helper.DummyData;
+                newData.title = "Download Completed!";
+                newData.year = "";
+                socket.emit('imdb-game', newData);
+            }
+            ScrapData();
         }
-    
-});
 
-
-app.get('/getlink', urlencodedParser, function (req, res) {
-
-   const url = 'tt0034583';
-
-    //give url as movie id and get back movie details
-    scraper.getDetails(url)
-    .then((data)=>{
-            //console.log(data);
-
-            
-            async function makeRequest() {
-                await storage.init( /* options ... */ );
-                await storage.setItem ('Currentdata', data );
-                console.log(data);
-                res.render ('index', {data: data,} );
-              }
-
-              makeRequest();
-
-    }).catch((err)=>{
-        console.log(err);
     });
-    
+
+
+
+    socket.on('Option', function (url) {
+
+
+        //scrap movie id's from this url
+        ListScraper(url, function (err, data) {
+            ScrabFunction(data);
+        });
+
+        //save scrapped data to storage
+        function ScrabFunction(data) {
+            async function ScrapData() {
+                await storage.init( /* options ... */);
+                await storage.setItem('Scrapedata', data)
+                console.log("List have been scrapped");
+
+                //render page with sending data
+
+                let newData = helper.DummyData;
+                newData.title = "Download Completed!";
+                newData.year = "";
+                socket.emit('imdb-game', newData);
+            }
+            ScrapData();
+        }
+
+    });
+
+    console.log('a user connected');
+    socket.on('disconnect', function () {
+        console.log('user disconnected');
+    });
 });
+
+
+
+// app.get('/getlink', urlencodedParser, function (req, res) {
+
+//    const url = 'tt0034583';
+
+//     //give url as movie id and get back movie details
+//     scraper.getDetails(url)
+//     .then((data)=>{
+//             //console.log(data);
+
+
+//             async function makeRequest() {
+//                 await storage.init( /* options ... */ );
+//                 await storage.setItem ('Currentdata', data );
+//                 console.log(data);
+//                 res.render ('index', {data: data,} );
+//               }
+
+//               makeRequest();
+
+//     }).catch((err)=>{
+//         console.log(err);
+//     });
+
+// });
 
 //while server is working on 3000 port give message
 http.listen(3000, () => {
